e1e5be230ac94ea1ce89e1c1f3fe97bc412674af
Shamil Gusseynov <42405526+Shamil-G@users.noreply.github.com>
2024-02-15 12:21:30 +0600

    Update spi2.c
    CLEAN  Code

--- Code/src/spi2.c
+++ Code/src/spi2.c
@@ -14,39 +14,7 @@
 
 extern unsigned long durationMs;
 
-
-void spi_gpio_init(void){
-  // SCK
-  InitGPio( SPI_SCK_Port, SPI_SCK_Pin, alternateF, push_pull, high, noPull, af5);
-  // MOSI TX DMA1 Stream4
-  InitGPio( SPI_MOSI_Port, SPI_MOSI_Pin, alternateF, push_pull, high, noPull, af5);
-  // MISO-PB14 RX DMA1 Stream4
-  InitGPio( SPI_MISO_Port, SPI_MISO_Pin, alternateF, push_pull, high, noPull, af5);
-
-#ifndef USE_SPI_ILI9341
-  // NSS-PB12
-  InitGPio( SPI_NSS_Port, SPI_NSS_Pin, alternateF, push_pull, high, noPull, af5);
-#endif
-}
-
-
-// Подключаем DMA1 к SPI2
-void spi2_dma_enable(){
-#ifdef USE_DMA
-  dma1_clear_error();
-  // SET_BIT(SPI2->CR2, SPI_CR2_RXDMAEN);
-  SET_BIT(SPI2->CR2, SPI_CR2_TXDMAEN);
-
-  //Enable Transfer complete interrupt Channel4
-  SET_BIT(DMA1_Stream4->CR, DMA_SxCR_TCIE);
-  //Enable Transfer error interrupt Channel4
-  SET_BIT(DMA1_Stream4->CR, DMA_SxCR_TEIE);
-
-  NVIC_EnableIRQ(DMA1_Stream4_IRQn);
-#endif
-}
-
-void dma_spi_init(SPI_TypeDef *spi){
+void dma_spi2_init(SPI_TypeDef *spi){
   // STM32F411, Page 170 of RM0383:
   // SPI1: DMA2
   // Channel_3
@@ -82,42 +50,77 @@ void dma_spi_init(SPI_TypeDef *spi){
   // Channel_5
   // Tx SPI5: DMA2_Stream5_IRQn interrupt
 	
-	
 //  DMA2_Stream2->CR = 0x0UL;
-	
-  //DMA controller clock enable
-  RCC->AHB1ENR |= RCC_AHB1ENR_DMA1EN;
-  // Установим адрес порта SPI куда DMA будет перекладывать данные
-  DMA1_Stream4->PAR = (uint32_t)&spi->DR;
-  //Выставим передачу из памяти в порт SPI
-  DMA1_Stream4->CR = 0UL | DMA_SxCR_DIR_0 | DMA_SxCR_MINC;
 
-  // Прерывание, кажется есть лишние, может все
-  DMA1_Stream4->CR |= DMA_SxCR_TCIE | //Transfer complete interrupt enable
-					  DMA_SxCR_TEIE | //TEIE: Transfer error interrupt enable
-					  DMA_SxCR_DMEIE; //Direct mode error interrupt enable
+  dma_init(DMA1, DMA1_Stream4);  // SPI DMA Tx
+  SET_BIT(DMA1_Stream4->CR, DMA_SxCR_CHSEL_2); // Выбор 4 канала
 
-  // Очищаем потенциальные прерывания
-  DMA1->HIFCR |= DMA_HIFCR_CTEIF4;
+  NVIC_EnableIRQ(DMA1_Stream4_IRQn);	
 }
 
-void spi2_dma_send(uint8_t *data, uint32_t len){
+void spi_init(SPI_TypeDef *spi){
+  // SCK
+  InitGPio( SPI_SCK_Port, SPI_SCK_Pin, alternateF, push_pull, high, noPull, af5);
+  // MOSI TX DMA1 Stream4
+  InitGPio( SPI_MOSI_Port, SPI_MOSI_Pin, alternateF, push_pull, high, noPull, af5);
+  // MISO-PB14 RX DMA1 Stream4
+  InitGPio( SPI_MISO_Port, SPI_MISO_Pin, alternateF, push_pull, high, noPull, af5);
+
+#ifndef USE_SPI_ILI9341
+  // NSS-PB12
+  InitGPio( SPI_NSS_Port, SPI_NSS_Pin, alternateF, push_pull, high, noPull, af5);
+#endif
+
+  // Включаем SPI
+  if(spi==SPI1)
+    RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
+  if(spi==SPI2)
+    RCC->APB1ENR |= RCC_APB1ENR_SPI2EN;
+	
+  // Выключаем все биты прерываний для работы с DMA
+  spi->CR1 = 0;
+  spi->CR2 = 0;
+//  SPI1->CR1 = SPI_CR1_BIDIMODE| //1: 1-line bidirectional data mode selected
+//	      SPI_CR1_BIDIOE; 	  //1: Output enabled (transmit-only mode)
+  spi->CR1 |=
+//	SPI_CR1_DFF 	| //1: 16-bit data frame format is selected for transmission/reception
+//	SPI_CR1_CPOL_Pos| //0: SCK=1 when idle, else SCK=0
+//	SPI_CR1_CPHA	| //1: Second edge is  is the MSBit capture strobe
+	SPI_CR1_SSM 	| // 1: NSS Software slave management enabled
+	SPI_CR1_SSI 	| // SSI: Internal slave select
+	// 0: SPI_CR1_BR_х - означает деление на 2 для f411 серии
+	// При использовании SPI2 на APB1ENR это означает 50MHz
+	// Baud rate (SPI_CR1_BR_2 | SPI_CR1_BR_1) = Fpclk/128 -> 400KHz
+	// Baud rate (SPI_CR1_BR_2 | SPI_CR1_BR_0) = Fpclk/64  -> 800KHz
+	// Baud rate (SPI_CR1_BR_2) = Fpclk/32  -> 1.6MHz
+//	SPI_CR1_BR_2 |
+//	SPI_CR1_BR_1 |
+	SPI_CR1_MSTR; // Master Configuration
+
+  // Включаем SPI
+  spi->CR1 |= SPI_CR1_SPE;
+}
+
+void spi2_dma_tx(uint8_t *data, uint32_t len){
   // Очищаем потенциальные прерывания, иначе не запустится
-  dmaStreamTx->CR &= ~DMA_SxCR_EN;
-  while(dmaStreamTx->CR & DMA_SxCR_EN);
+  // DMA1_Stream4->CR &= ~DMA_SxCR_EN;
+  // while(DMA1_Stream4->CR & DMA_SxCR_EN);
 
-  dma4spi->HIFCR &= ~DMA_HIFCR_CHTIF4;
-  dma4spi->HIFCR &= ~DMA_HIFCR_CTCIF4;
-  dma4spi->HIFCR &= ~DMA_HIFCR_CFEIF4;
-
+  DMA1_Stream4->CR |= DMA_SxCR_DIR_0 |   // Направление данных из памяти в периферию
+			DMA_SxCR_MINC;     // Инкремент памяти включен
+  // Установим адрес порта SPI куда DMA будет перекладывать данные
+  DMA1_Stream4->PAR = (uint32_t)&SPI2->DR;
   // Заносим адрес памяти откуда мы будем передавать данные
-  dmaStreamTx->M0AR = (uint32_t)data;
-
+  DMA1_Stream4->M0AR = (uint32_t)data;
   // Количество передаваемых данных
-  dmaStreamTx->NDTR = len;
-  dmaStreamTx->CR &= ~DMA_SxCR_MSIZE;
+  DMA1_Stream4->NDTR = len;
+  // По умолчанию стоит 1 байт
+  // dmaStreamTx->CR &= ~DMA_SxCR_MSIZE;
 
-  dmaStreamTx->CR |= DMA_SxCR_EN;
+  //Сообщаем SPI что надо использовать DMA при передаче
+  spi->CR2 |= SPI_CR2_TXDMAEN;
+
+  DMA1_Stream4->CR |= DMA_SxCR_EN;
 }
 
 uint8_t SPI2_WriteData(uint8_t* pData, uint32_t Size, uint32_t Timeout)
@@ -177,70 +180,22 @@ uint8_t SPI2_WriteData(uint8_t* pData, uint32_t Size, uint32_t Timeout)
   return 0;
 }
 
-void spi_init(SPI_TypeDef *spi){
-  // Включаем SPI
-  if(spi==SPI1)
-    RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
-  if(spi==SPI2)
-    RCC->APB1ENR |= RCC_APB1ENR_SPI2EN;
-
-  // Выключаем все биты прерываний для работы с DMA
-  spi->CR2 = 0;
-//  SPI1->CR1 = SPI_CR1_BIDIMODE| //1: 1-line bidirectional data mode selected
-//	      SPI_CR1_BIDIOE; 	  //1: Output enabled (transmit-only mode)
-  spi->CR1 = 0;
-  spi->CR1 |=
-//	SPI_CR1_DFF 	| //1: 16-bit data frame format is selected for transmission/reception
-//	SPI_CR1_CPOL_Pos| //0: SCK=1 when idle, else SCK=0
-//	SPI_CR1_CPHA	| //1: Second edge is  is the MSBit capture strobe
-	SPI_CR1_SSM 	| // 1: NSS Software slave management enabled
-	SPI_CR1_SSI 	| // SSI: Internal slave select
-	// 0: SPI_CR1_BR_х - означает деление на 2 для f411 серии
-	// При использовании SPI2 на APB1ENR это означает 50MHz
-	// Baud rate (SPI_CR1_BR_2 | SPI_CR1_BR_1) = Fpclk/128 -> 400KHz
-	// Baud rate (SPI_CR1_BR_2 | SPI_CR1_BR_0) = Fpclk/64  -> 800KHz
-	// Baud rate (SPI_CR1_BR_2) = Fpclk/32  -> 1.6MHz
-//	SPI_CR1_BR_2 |
-//	SPI_CR1_BR_1 |
-	SPI_CR1_MSTR; // Master Configuration
-
-  // TI Frame format, этот бит отменяет важность CPOL & CPHA & SSM & SSI
-  // spi->CR2 |= SPI_CR2_FRF;
-
-  //Сообщаем SPI что надо использовать DMA при передаче
-#ifdef USE_DMA
-      spi->CR2 |= SPI_CR2_TXDMAEN;
-#endif
-
-  //Сообщаем SPI что надо использовать DMA при приеме
-  //  spi->CR2 |= SPI_CR2_RXDMAEN;
-
-  // Включаем SPI
-  // spi->CR1 |= SPI_CR1_SPE;
-}
-
-void dma1_streamDisable(){
-//  DMA1_Stream3->CR &= ~DMA_SxCR_EN;
-  DMA1_Stream4->CR &= ~DMA_SxCR_EN;
-}
-
-void dma1_clear_error(){
-  WRITE_REG(DMA1->HIFCR, DMA_HIFCR_CTCIF4);
-  WRITE_REG(DMA1->HIFCR, DMA_HIFCR_CTEIF4);
-  WRITE_REG(DMA1->HIFCR, DMA_HIFCR_CFEIF4);
-  WRITE_REG(DMA1->HIFCR, DMA_HIFCR_CHTIF4);
-}
-
 // SPI2_TX -> DMA1, Channel 0, Stream 4
 void DMA1_Stream4_IRQHandler(void)
 {
   if( READ_BIT(DMA1->HISR, DMA_HISR_TCIF4) != 0 )
   {
     //Clear Channel 4 interrupt flag
-    WRITE_REG(DMA1->HIFCR, DMA_HIFCR_CTCIF4);
-    __NOP();  }
+    DMA1->HIFCR |= DMA_HIFCR_CTCIF4;
+    __NOP();  
+  }
   else{
-	dma1_clear_error();
+	DMA1_Stream4->CR &= ~DMA_SxCR_EN;
+	DMA1->HIFCR |= 	DMA_HIFCR_CHTIF4 | // Stream x clear half transfer interrupt flag
+			DMA_HIFCR_CTEIF4 | // Stream x clear transfer error interrupt flag
+			DMA_HIFCR_CDMEIF4| // Stream x clear direct mode error interrupt flag
+			DMA_HIFCR_CFEIF4;  // Stream x clear FIFO error interrupt flag
+	  
       __NOP();
   }
 }
